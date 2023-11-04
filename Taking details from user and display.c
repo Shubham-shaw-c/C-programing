260ed499348199c0fec85c6290215c593dbc5da0
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-04 18:41:54 +0530

    Taking detail from user and display

--- Taking details from user and display.c
+++ Taking details from user and display.c
@@ -3,13 +3,13 @@ int main(){
     char name,program,mail_id;
     int Birth_year,Current_year;
     printf("Enter the Name\n");
-    scanf("%c",&name);
+    scanf("%s",&name);
     printf("Enter the Birth year\n");
     scanf("%d",&Birth_year);
     printf("Enter the Program\n");
-    scanf("%c",&program);
+    scanf("%s",&program);
     printf("Enter the mail Id\n");
-    scanf("%c",&mail_id);
+    scanf("%s",&mail_id);
     printf("Enter the Current year\n");
     scanf("%d",&Current_year);
     printf("User age is = %d ",Current_year-Birth_year);
