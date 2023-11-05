3abadec45e9be3c513ff08f0d3f88e834e3d23ca
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-05 12:03:31 +0530

    Right angled triangle or not ?

---
+++ Right angled triangle or not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int side1, side2, side3;
+    printf("ente the side one = ");
+    scanf("%d", &side1);
+    printf("ente the side two = ");
+    scanf("%d", &side2);
+    printf("ente the side three = ");
+    scanf("%d", &side3);
+    if ((side1 * side1) + (side2 * side2) == (side3 * side3) || (side1 * side1) + (side3 * side3) == (side2 * side2) ||
+        (side2 * side2) + (side3 * side3) == (side1 * side1)) {
+        printf("the traingle is right angled triagle");
+    } else {
+        printf("the triangle is not right angled triangle");
+    }
+}
