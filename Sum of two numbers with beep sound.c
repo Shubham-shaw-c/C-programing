0999ccbbe313a945a5156600b34188f0a231d916
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-13 10:59:05 +0530

    Sum of two numbers with beep sound .

---
+++ Sum of two numbers with beep sound.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,sum;
+    printf("Enter the first number\n \a");
+    scanf("%d",&num1);
+    printf("Enter the second number\n \a");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("Sum is = %d \n \a",sum);
+
+}
