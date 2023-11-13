ec67b8408830a4d9834b67e077d0b472de35a8a1
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-13 11:12:55 +0530

    Check whether a character is vowel or not .

---
+++ check whether a character is vowel or not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter the character =");
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
+        printf("%c is a vowel",ch);
+    }else{
+        printf("%c is not a vowel",ch);
+    }
+}
