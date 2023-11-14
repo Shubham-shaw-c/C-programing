c88621061f4d43b63eac52164b3155d19aad6e04
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-15 02:54:49 +0530

    Check whether water can reach boiling point or not .

---
+++ Check whether water can reach boiing point or not .c
@@ -0,0 +1,12 @@
+// Created by SHUBHAM on 15-11-2023.
+#include<stdio.h>
+int main(){
+    float temp;
+    printf("Enter the temperature \n");
+    scanf("%f",&temp);
+    if(temp>=100){
+        printf("The water starts Boiling");
+    }else{
+        printf("The water will not starts Boiling");
+    }
+}
