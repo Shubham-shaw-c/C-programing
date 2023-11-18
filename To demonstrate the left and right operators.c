60aa93a1a2194397965ae1bb4835ace2111c2aa9
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-18 11:44:38 +0530

    To demonstrate the left and right shift operators .

---
+++ To demonstrate the left and right operators.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    printf("right shift operators= %d\n",a>>1);
+    printf("left shift operators= %d\n",a<<1);
+}
