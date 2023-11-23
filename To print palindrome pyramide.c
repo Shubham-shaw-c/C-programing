326f6c42a1bcf9b2e7fdef342c2d5c61072ecea3
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-23 19:52:54 +0530

    To print fibonacci series in pattern

---
+++ To print palindrome pyramide.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i,j,a=0;
+    for(i=1;i<4;i++) {
+        for (j = i; j < 4; j++) {
+            printf(" ");
+        }
+    for(j=1;j<1;j++){
+        printf("%d",a);
+        a--;
+    }
+    }printf("\n");
+    a++;
+}
--- To print the multiplication table of n.c
+++ To print the multiplication table of n.c
@@ -4,7 +4,6 @@ int main(){
     printf("Enter the digit you want to print the multiplication\n");
     scanf("%d",&n);
     for(int i=1;i<=10;i++){
-        //for(int j=1;j<=i;j--){
         printf("%d*%d=%d\n",n,i,i*n);
         }
     printf("-------n");
