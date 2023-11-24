9a644004d04bd8d73b9bc29654e2b12cc78eae25
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-24 23:32:40 +0530

    To print inverted full pyramid

---
+++ To print inverted full pyramid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int row;
+    printf("Enter the size to print the pattern \n");
+    scanf("%d",&row);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < 2 * i; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * (row - i) - 1; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
