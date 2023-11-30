e35ce7216e8c094f24ae48dc0bbce7c08b39b89d
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-11-30 22:46:51 +0530

    To check a number is Armstrong number or not .

--- Convert decimal number into binary number  .c
+++ Convert decimal number into binary number  .c
@@ -4,8 +4,7 @@ int main()
     int d,b= 0,i = 1,remainder;
     printf("Enter a decimal number: ");
     scanf("%d", &d);
-    while (d != 0)
-    {
+    while (d != 0){
         remainder = d % 2;
         d/= 2;
         b+= remainder * i;
