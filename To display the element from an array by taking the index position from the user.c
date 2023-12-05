1315c3ea63ee3a798b63c60a8ba2424f0dd9f13b
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-12-05 20:42:31 +0530

    To display the element from an array by taking the index position from the user .

---
+++ To display the element from an array by taking the index position from the user.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int size,j;
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+    int num[size];
+    for(int i=0;i<size;i++){
+        printf("Enter index element no:%d\n",i);
+        scanf("%d",&num[i]);
+    }
+    printf("Enter the index no. :");
+    scanf("%d",&j);
+    printf("%d",num[j]);
+}
