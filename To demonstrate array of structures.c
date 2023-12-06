af9ee0939e5bab4cc22199310bb2c077fb1cd0c4
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2023-12-06 19:34:12 +0530

    To demonstrate array of structures .

--- To demonstrate array of structures.c
+++ To demonstrate array of structures.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main(){
-    struct employee{
+int main()
+{
+    struct employee
+    {
         char name[100];
         int age;
         float salary;
@@ -9,13 +11,13 @@ int main(){
     int counter, index, count, totalSalary;
     printf("Enter Number of Employees\n");
     scanf("%d", &count);
-    for(counter=0; counter<count; counter++){
+    for(counter=0; counter<count; counter++)
+    {
         printf("Enter Name of Employee\n");
         getchar();
         gets(em[counter].name);
         printf("Enter the Age and Salary of Employee\n");
         scanf("%d %f", &em[counter].age, &em[counter].salary);
     }
-    printf("Average Salary of an Employee is %f\n",
-           (float)totalSalary/count);
+    printf("Average Salary of an Employee is %f\n",(float)totalSalary/count);
 }
